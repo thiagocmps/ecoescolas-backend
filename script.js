51301f9573b7fad09295e2f2c445fbb60aa5522a
Thiago Campos Vidal de Souza <thiagocvs2003@gmail.com>
2025-04-01 19:34:08 +0100

    adiciona arquivos de configuração e dependências para dotenv e mongoose

---
+++ script.js
@@ -0,0 +1,27 @@
+//connect to db on mongodb atlas
+require("dotenv").config();
+const mongoose = require("mongoose");
+const express = require("express");
+/* const cors = require('cors'); */
+/* const utilities = require("./utilities/utilities"); */
+const urimongodb = process.env.MONGO_URI;
+const port = process.env.PORT || 5000;
+const app = express();
+
+app.use(express.json());
+/* app.use(utilities.auth); */
+/* app.use(cors()); */
+
+app.listen(port, () => {
+  console.log("Server is running on port " + port);
+  mongoose.connect(urimongodb);
+});
+
+mongoose.connection.on(
+  "error",
+  console.error.bind(console, "connection error:")
+);
+
+mongoose.connection.once("open", function () {
+  console.log("Base de dados conectada");
+});
---
+++ utilities/utilities.js
@@ -0,0 +1,71 @@
+var jwt = require("jsonwebtoken");
+const modelUsers = require("../Models/model_users.js");
+let secret = "%)$2sF55Idf(Rm&jyPnkqAL^+8m4dSw)";
+
+const generateToken = (user_info, callback) => {
+  let token = jwt.sign(
+    {
+      data: user_info,
+    },
+    secret,
+    { expiresIn: "1000h" }
+  );
+  return callback(token);
+};
+
+
+const validateToken = (token, callback) => {
+  if (!token) {
+    return callback(false, null);
+  }
+  jwt.verify(token.replace("Bearer ", ""), secret, function (error, decoded) {
+    if (error) {
+      return callback(false, null);
+    }
+    console.log("Decoded: ", decoded);
+    let loggedUser = decoded.data.username;
+    console.log("User Authorized: " + loggedUser);
+    console.log("Token: " + token);
+    modelUsers
+      .findOne({ username: loggedUser })
+      .then(user => {
+        if (!user) {
+          return callback(false, null);
+        }
+        console.log("User founded:", user);
+        return callback(true, user);
+      })
+  });
+};
+
+
+const isAdmin = function(req, res, next) {
+  if (req.loggedUser.role === "admin") {
+    return next();
+  } else {
+    return res.status(401).json({ message: "Not authorized" });
+  }
+};
+
+
+const auth = function(req, res, next) {
+    let exception = ["/users/login", "/users/register"];
+    if (exception.indexOf(req.url) >= 0) {
+      return next();
+    } else {
+      validateToken(req.headers.authorization, (result, user) => {
+        if (result) {
+          req.loggedUser = user;
+          console.log("logged user: " + user);
+          return next();
+        } else {
+          return res.status(401).json({ message: "Not authorized" });
+        }
+      });
+    }
+} 
+
+exports.isAdmin = isAdmin;
+exports.generateToken = generateToken;
+exports.validateToken = validateToken;
+exports.auth = auth;
