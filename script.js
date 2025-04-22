16fbb6f3df58658baf24f18faca5e9b78b797ea2
Thiago Campos Vidal de Souza <thiagocvs2003@gmail.com>
2025-04-22 17:55:09 +0100

    Refatora configuração do CORS para permitir múltiplas origens e métodos. Remove chamada redundante ao middleware CORS.

--- script.js
+++ script.js
@@ -2,23 +2,31 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
 const express = require("express");
-const cors = require('cors');
+const cors = require("cors");
 const utilities = require("./utilities/utilities");
 const user_route = require("./Routes/user_route");
 const urimongodb = process.env.MONGO_URI;
 const port = process.env.PORT || 5000;
 const app = express();
 
+app.use(cors({
+  origin: ['http://localhost:8081', 'https://seu-app.koyeb.app', 'http://localhost:5000', "http://192.168.1.217:5000", 'https://ashamed-elfrida-thiagocmps-a0126839.koyeb.app/users/login', "http://192.168.1.217:8081","https://ashamed-elfrida-thiagocmps-a0126839.koyeb.app"], // ou um array: ['http://localhost:8081', 'http://192.168.1.217:8081']
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true
+}));
+app.options('*', cors());
 app.use(express.json());
 app.use(utilities.auth);
 app.use("/users", user_route);
 
-app.use(cors());
-
-app.listen(port,/*  '0.0.0.0', */ () => {
-  console.log("Server is running on port " + port);
-  mongoose.connect(urimongodb);
-});
+app.listen(
+  port,
+  /*  '0.0.0.0', */ () => {
+    console.log("Server is running on port " + port);
+    mongoose.connect(urimongodb);
+  }
+);
 
 mongoose.connection.on(
   "error",
