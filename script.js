9ae6866ee293e7aeab62c1b77d8ab21423093f04
Thiago Campos Vidal de Souza <thiagocvs2003@gmail.com>
2025-04-24 23:00:12 +0100

    Atualiza configuração do CORS para permitir origens múltiplas, incluindo wildcard '*'.

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 5000;
 const app = express();
 
 app.use(cors({
-  origin: ['http://localhost:8081', 'https://seu-app.koyeb.app', 'http://localhost:5000', "http://192.168.1.217:5000", 'https://ashamed-elfrida-thiagocmps-a0126839.koyeb.app/users/login', "http://192.168.1.217:8081","https://ashamed-elfrida-thiagocmps-a0126839.koyeb.app"], // ou um array: ['http://localhost:8081', 'http://192.168.1.217:8081']
+  origin: ['http://localhost:8081', 'https://seu-app.koyeb.app', '*', 'http://localhost:5000', "http://192.168.1.217:5000", 'https://ashamed-elfrida-thiagocmps-a0126839.koyeb.app/users/login', "http://192.168.1.217:8081","https://ashamed-elfrida-thiagocmps-a0126839.koyeb.app"], // ou um array: ['http://localhost:8081', 'http://192.168.1.217:8081']
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true
