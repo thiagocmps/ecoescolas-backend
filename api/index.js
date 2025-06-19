f84820106eddf6f887896112d08defe7893647cb
Thiago Campos Vidal de Souza <thiagocvs2003@gmail.com>
2025-06-19 20:35:50 +0100

    Refactor: Reorganizar código e adicionar middleware para conexão com MongoDB

--- api/index.js
+++ api/index.js
@@ -1,48 +1,62 @@
-//connect to db on mongodb atlas
-require("dotenv").config();
-const mongoose = require("mongoose");
 const express = require("express");
+const serverless = require("serverless-http");
+const mongoose = require("mongoose");
 const cors = require("cors");
 const utilities = require("../utilities/utilities");
 const userRoute = require("../Routes/users_routes");
 const activityRoute = require("../Routes/activities_routes");
 const reportsRouter = require("../Routes/reports_routes");
+
+require("dotenv").config();
+
 const urimongodb = process.env.MONGO_URI;
-const port = process.env.PORT || 5000;
+
 const app = express();
 
+// Configuração de CORS
 app.use(
   cors({
     origin: [
       "http://localhost:8081",
       "http://localhost:5000",
       "http://192.168.1.217:8081",
-    ], // ou um array: ['http://localhost:8081', 'http://192.168.1.217:8081']
+    ],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   })
 );
-/* 
-app.options(cors()); */
+
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
+
+// Conecta ao MongoDB apenas uma vez
+let isConnected = false;
+
+async function connectDB() {
+  if (!isConnected) {
+    try {
+      await mongoose.connect(urimongodb);
+      console.log("Base de dados conectada");
+      isConnected = true;
+    } catch (err) {
+      console.error("Erro ao conectar ao MongoDB:", err);
+    }
+  }
+}
+
+// Middleware para garantir conexão ativa
+app.use(async (req, res, next) => {
+  await connectDB();
+  next();
+});
+
+// Middleware de autenticação e rotas
 app.use(utilities.auth);
 app.use("/users", userRoute);
 app.use("/activities", activityRoute);
 app.use("/reports", reportsRouter);
 
-
-app.listen(port, () => {
-  console.log("Server is running on port " + port);
-  mongoose.connect(urimongodb);
-});
-
-mongoose.connection.on(
-  "error",
-  console.error.bind(console, "connection error:")
-);
-
-mongoose.connection.once("open", function () {
-  console.log("Base de dados conectada");
-});
+// Exporta como função Serverless
+module.exports = app;
+module.exports.handler = serverless(app);
